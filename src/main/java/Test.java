b18184af2dcd04ff8f485f849bff64bffc8888d0
TSAREV <tsarev-dmitriy@bk.ru>
2021-05-27 11:57:24 +0300

    getFactorial1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,17 +1,17 @@
 public class Test {
 
     public static void main(String[] args) {
-        getFactorial(3);
+        getFactorial1(3);
     }
 
     //считаем факториал числа
     ///для практики
-    public static int getFactorial(int f) {
+    public static int getFactorial1(int f) {
         if (f <= 1) {
             return 1;
         }
         else {
-            return f * getFactorial(f - 1);
+            return f * getFactorial1(f - 1);
         }
     }
 
