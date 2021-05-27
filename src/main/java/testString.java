1fe443a50838237f7031d9a5926c43e165d89eb1
TSAREV <tsarev-dmitriy@bk.ru>
2021-05-27 13:36:49 +0300

    add new class

---
+++ src/main/java/testString.java
@@ -0,0 +1,8 @@
+public class testString {
+    public static void main(String[] args) {
+        String a = "Hello";
+        a = "Hello1";
+//         a = new String("Hi");
+        System.out.println(a);
+    }
+}
